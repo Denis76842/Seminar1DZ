8739832d38fdf1e144b5338794bc6b59ca56603a
Denis76842 <112509983+Denis76842@users.noreply.github.com>
2022-09-05 17:24:13 +0400

    Задача 2

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) 
+{
+    Console.WriteLine("Первое число больше");
+
+}
+
+else if (number1 < number2)
+
+{
+    Console.WriteLine("Первое число меньше");
+
+}
+else
+ {
+Console.WriteLine("Числа равны");
+
+}
+
+//The end.
